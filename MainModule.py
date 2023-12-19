818dbf76710688fc66bf4e9417278d7d7811720d
Mateusz <wanmatedzlewicz@gmail.com>
2023-12-19 13:58:25 +0100

    adjusted and shortened script

--- MainModule.py
+++ MainModule.py
@@ -4,14 +4,13 @@ import speech_recognition as sr
 from datetime import datetime
 import json
 import requests
-import time
 
 def listen_microphone():
     recognizer = sr.Recognizer()
 
     with sr.Microphone() as source:
         print("Listening...")
-        recognizer.adjust_for_ambient_noise(source, duration=1)
+        recognizer.adjust_for_ambient_noise(source)
         audio = recognizer.listen(source)
 
     return audio
@@ -38,7 +37,7 @@ def main():
                 get_current_weather()
 
         except sr.UnknownValueError:
-            print("Could not understand audio")
+            print("...")
         except sr.RequestError as e:
             print(f"Error with the speech recognition service; {e}")
 
@@ -57,83 +56,6 @@ def get_bitcoin_price():
         print("Current Bitcoin price:", price)
     else:
         print("Failed to retrieve Bitcoin price. Check your API credentials.")
-
-    endpoint = "https://api.bybit.com/v2/private/wallet/balance"
-
-    # Generate the required headers, including timestamp and API signature
-    headers = generate_bybit_headers(api_key, api_secret)
-
-    try:
-        # Make the API request
-        response = requests.get(endpoint, headers=headers)
-        response.raise_for_status()
-
-        # Parse the JSON response
-        data = response.json()
-        print("API Response:", data)  # Print the entire response
-
-        # Modify the following line based on the actual structure of the JSON response
-        balance = data["result"]["equity"]
-        print("Your Bybit balance is:", balance)
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error making API request: {e}")
-    except json.JSONDecodeError as e:
-        print(f"Error decoding JSON response: {e}")
-    except KeyError as e:
-        print(f"Error accessing wallet balance: {e}")
-
-    import hashlib
-    import hmac
-    import time
-
-    # Generate a timestamp and prepare the request path
-    timestamp = str(int(time.time() * 1000))
-    path = "/v2/private/wallet/balance"
-
-    # Concatenate the components to create the string to sign
-    sign_string = f"{path}{timestamp}"
-    
-    # Generate the API signature using HMAC-SHA256
-    signature = hmac.new(api_secret.encode(), sign_string.encode(), hashlib.sha256).hexdigest()
-
-    # Construct the headers
-    headers = {
-        "Content-Type": "application/json",
-        "api-key": api_key,
-        "timestamp": timestamp,
-        "sign": signature,
-    }
-
-    return headers
-    endpoint = "https://api.bybit.com/v2/private/wallet/balance"
-
-    # Set your API keys
-    headers = {
-        "Content-Type": "application/json",
-        "api_key": api_key,
-        "api_secret": api_secret,
-    }
-
-    try:
-        # Make the API request
-        response = requests.get(endpoint, headers=headers)
-        response.raise_for_status()
-
-        # Parse the JSON response
-        data = response.json()
-        print("API Response:", data)  # Print the entire response
-
-        # Modify the following line based on the actual structure of the JSON response
-        balance = data["result"]["equity"]
-        print("Your Bybit balance is:", balance)
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error making API request: {e}")
-    except json.JSONDecodeError as e:
-        print(f"Error decoding JSON response: {e}")
-    except KeyError as e:
-        print(f"Error accessing wallet balance: {e}")
 
 def load_api_keys():
     with open("api_keys.json", "r") as file:
