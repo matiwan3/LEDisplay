efc6b7aa6e19160e25be9e21bfe67bd682b0d779
Mateusz <wanmatedzlewicz@gmail.com>
2023-12-19 13:47:47 +0100

    renamed files and added weather function

--- MainModule.py
+++ MainModule.py
@@ -4,6 +4,7 @@ import speech_recognition as sr
 from datetime import datetime
 import json
 import requests
+import time
 
 def listen_microphone():
     recognizer = sr.Recognizer()
@@ -11,16 +12,11 @@ def listen_microphone():
     with sr.Microphone() as source:
         print("Listening...")
         recognizer.adjust_for_ambient_noise(source, duration=1)
-        audio = recognizer.listen(source, timeout=4)
+        audio = recognizer.listen(source)
 
     return audio
 
 def main():
-    recognizer = sr.Recognizer()
-    api_keys = load_api_keys()
-    api_key = api_keys["api_key"]
-    api_secret = api_keys["secret_key"]
-
     while True:
         audio = listen_microphone()
 
@@ -31,12 +27,15 @@ def main():
                 print("Hello!")
             elif "bye-bye alvin" in text:
                 print("Goodbye!")
+                break
+            elif "i love you alvin" in text:
+                print("<3")    
             elif "alvin what is the bitcoin price" in text:
                 get_bitcoin_price()
-            # elif "alvin what is my crypto wallet balance" in text:
-                # get_bybit_balance(api_key, api_secret)
             elif "alvin what time is it" in text:
                 print("Current time is:", datetime.now().strftime("%H:%M:%S"))
+            elif "alvin what is the weather" in text:
+                get_current_weather()
 
         except sr.UnknownValueError:
             print("Could not understand audio")
@@ -59,7 +58,6 @@ def get_bitcoin_price():
     else:
         print("Failed to retrieve Bitcoin price. Check your API credentials.")
 
-def get_bybit_balance(api_key, api_secret):
     endpoint = "https://api.bybit.com/v2/private/wallet/balance"
 
     # Generate the required headers, including timestamp and API signature
@@ -85,7 +83,6 @@ def get_bybit_balance(api_key, api_secret):
     except KeyError as e:
         print(f"Error accessing wallet balance: {e}")
 
-def generate_bybit_headers(api_key, api_secret):
     import hashlib
     import hmac
     import time
@@ -143,5 +140,26 @@ def load_api_keys():
         api_keys = json.load(file)
     return api_keys
 
+def get_current_weather():
+    endpoint = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Pozna%C5%84?unitGroup=metric&key=R7GHTFU8ZTHTXLJFQBWLLF2YK&contentType=json"
+    response = requests.get(endpoint)
+
+    if response.status_code == 200:
+        data = response.json()
+        current_datetime = datetime.now().strftime("%H:00:00")
+
+        current_hour_data = next((hour for hour in data["days"][0]["hours"] if hour["datetime"] == current_datetime), None)
+
+        if current_hour_data:
+            current_temp = current_hour_data["temp"]
+            current_conditions = current_hour_data["conditions"]
+            print(f"Current weather at {current_datetime}: Temperature: {current_temp}°C, Conditions: {current_conditions}")
+        else:
+            print("Unable to retrieve current weather data.")
+
+    else:
+        print("Failed to retrieve weather data. Check the API endpoint and key.")
+
 if __name__ == "__main__":
+    recognizer = sr.Recognizer()
     main()
